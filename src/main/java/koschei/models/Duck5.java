c11bfd9e30dc701eccc6ca5f811ef1e9ab9fc527
jmd1x <144138299+jmd1x@users.noreply.github.com>
2023-11-08 15:37:21 +0300

    Version 1.1

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    private final Egg6 egg;
-
     @Autowired
+    private Egg6 egg;
+
     public Duck5(Egg6 egg) {
         this.egg = egg;
     }
